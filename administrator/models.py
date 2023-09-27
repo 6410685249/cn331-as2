d86f04ffa5ea401fdbf16077b6fd7f62d70e2b02
Siriphatson Sringamphong <6410685249@cn330>
2023-09-28 01:11:51 +0700

    add semester, year & ser primary key

--- administrator/models.py
+++ administrator/models.py
@@ -4,7 +4,9 @@ from django.db import models
 
 class Subject(models.Model):
     name = models.CharField(max_length=64)
-    code = models.CharField(max_length=6)
+    code = models.CharField(primary_key=True, max_length=6)
     professor = models.CharField(max_length=128)
+    semester = models.IntegerField()
+    year = models.CharField(max_length=4)
     quota = models.BooleanField(default=False)
     seat = models.IntegerField()
