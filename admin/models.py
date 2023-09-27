b0fa0d301956a05b15544e66c0d177003a0bcc13
Chalisa Thummaraj <6410685041@cn331>
2023-09-28 00:15:21 +0700

    Delete admin

--- admin/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Subject(models.Model):
-    name = models.CharField(max_length=64)
-    code = models.CharField(max_length=6)
-    professor = models.CharField(max_length=128)
-    quota = models.IntegerField(default=0)
-    seat = models.IntegerField()
