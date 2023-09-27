69d3fb46000aa5a1966ba0b6c6e90b1d71acd539
Siriphatson Sringamphong <6410685249@cn330>
2023-09-28 00:26:37 +0700

    delete class Subject

--- users/models.py
+++ users/models.py
@@ -1,17 +1,10 @@
 from django.db import models
-# from ..admin.models import Subject
+from ..administrator.models import Subject
 # Create your models here.
-
-class Subject(models.Model):
-    name = models.CharField(max_length=64)
-    code = models.CharField(max_length=6)
-    professor = models.CharField(max_length=128)
-    quota = models.IntegerField(default=0)
-    seat = models.IntegerField()
 
 class Student(models.Model):
     name = models.CharField(max_length=64)
     surname = models.CharField(max_length=64)
     id = models.IntegerField(primary_key=True)
     status = models.CharField(max_length=20)
-    # subjects = models.ManyToManyField(Subject, blank=True, related_name="list Subjects")
+    subjects = models.ManyToManyField(Subject, blank=True, related_name="list Subjects")
