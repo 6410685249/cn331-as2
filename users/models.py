8bc07373837a7cc4867e0dc4cbd1b2a74bb92b39
Chalisa Thummaraj <6410685041@cn331>
2023-09-28 02:05:51 +0700

    edit model by adding faculty in student

--- users/models.py
+++ users/models.py
@@ -3,8 +3,21 @@ from django.db import models
 
 # Create your models here.
 class Student(models.Model):
+    FACULTY = [
+        ('ทันตแพทย์','Dentistry'),
+        ('พยาบาลศาสตร์','Nursing'),
+        ('แพทยศาสตร์','Medicine'),
+        ('เภสัชศาสตร์','Pharmaceutical Sciences'),
+        ('ศึกษาศาสตร์','Education'),
+        ('วิทยาศาสตร์','Science'),
+        ('วิศวกรรมศาสตร์','Engineering'),
+        ('สถาปัตยศาสตร์','Architecture'),
+        ('นิเทศศาสตร์','Communication Arts'),
+        ('นิติศาสตร์','Law'),
+    ]
     name = models.CharField(max_length=64)
     surname = models.CharField(max_length=64)
     id = models.CharField(primary_key=True, max_length=10)
     status = models.BooleanField(default=False)
+    faculty = models.CharField(max_length=128, choices=FACULTY, default="null")
     # subjects = models.ManyToManyField(Subject, blank=True, related_name="list Subjects")
