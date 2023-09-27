6138275e69d67cc37880b30a448e70a5a0228267
Siriphatson Sringamphong <6410685249@cn330>
2023-09-28 00:32:15 +0700

    comment list subject

--- users/models.py
+++ users/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-from ..administrator.models import Subject
-# Create your models here.
+# from administrator.models import Subject
 
+# Create your models here.
 class Student(models.Model):
     name = models.CharField(max_length=64)
     surname = models.CharField(max_length=64)
     id = models.IntegerField(primary_key=True)
     status = models.CharField(max_length=20)
-    subjects = models.ManyToManyField(Subject, blank=True, related_name="list Subjects")
+    # subjects = models.ManyToManyField(Subject, blank=True, related_name="list Subjects")
