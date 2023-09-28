4a9ba333e6a633aaf35965e54d9a2cd8650d60fb
Chalisa Thummaraj <6410685041@cn331>
2023-09-29 02:49:15 +0700

    edit path for quota in reg

--- reg/views.py
+++ reg/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from administrator.models import Subject
+from users.models import Student
 
 # Create your views here.
+
+# def quota(request):
+#     return render(request, 'index.html')
+
+'''
+    ปุ่มที่กดเพิ่มนำรายวิชาโควต้าที่เลือกส่งเข้าไปใน list_subject 
+'''
+def Add_Quota(request, code):
+    subject = Subject.objects.get(code=code)
+
+    return render(request)
+
+# def Delete_Quota(request):
+# def Add_Subject(request):
+# def Delete_Subject(request):
+
+def Quota(request):
+    student = Student.objects.get(user_id=request.user)
+    student_subjects = student.subjects.all()
+    return render(request, 'Quota.html', {
+        'subjects' : student_subjects,
+        'count' : len(student_subjects),
+    })
