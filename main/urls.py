1a1a75ad02a65e435b29b2dcdb103ca6cdf3fcb5
Chalisa Thummaraj <6410685041@cn331>
2023-09-28 21:28:29 +0700

    Edit path to login

--- main/urls.py
+++ main/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', include('users.urls')),
+    path('', include('users.urls')),
     # path('register/', include('reg.urls')),
 ]
 
