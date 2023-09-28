b0c32b2e0255096702421740830200c3ce13ffad
Siriphatson Sringamphong <6410685249@cn330>
2023-09-28 22:47:58 +0700

    users connect with student

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from administrator.models import Subject
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Student(models.Model):
@@ -15,9 +16,10 @@ class Student(models.Model):
         ('นิเทศศาสตร์','Communication Arts'),
         ('นิติศาสตร์','Law'),
     ]
-    name = models.CharField(max_length=64)
-    surname = models.CharField(max_length=64)
-    id = models.CharField(primary_key=True, max_length=10)
+    # name = models.CharField(max_length=64)
+    # surname = models.CharField(max_length=64)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    student_id = models.CharField(primary_key=True, max_length=10)
     status = models.BooleanField(default=False)
     faculty = models.CharField(max_length=128, choices=FACULTY, default="null")
     subjects = models.ManyToManyField(Subject, blank=True, related_name="list_Subjects")
